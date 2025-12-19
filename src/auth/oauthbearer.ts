07bf8460b34962fd825ac22be535a23d79b30348
Priit Käärd <priitkaard123@gmail.com>
2025-12-19 16:02:12 +0200

    feat: oauthbearer auth mechanism

---
+++ src/auth/oauthbearer.ts
@@ -0,0 +1,83 @@
+import { API } from '../api';
+import { SASLProvider } from '../broker';
+import { exponentialBackoff, withRetry } from '../utils/retry';
+
+export const oAuthBearer = (getToken: () => Promise<{ access_token: string }>): SASLProvider => {
+    return {
+        mechanism: 'OAUTHBEARER',
+        authenticate: async ({ sendRequest }) => {
+            const { access_token: accessToken } = await getToken();
+
+            const sep = String.fromCharCode(1);
+            const authBytes = `n,,${sep}auth=Bearer ${accessToken}${sep}${sep}`;
+            await sendRequest(API.SASL_AUTHENTICATE, { authBytes: Buffer.from(authBytes) });
+        },
+    };
+};
+
+export const oAuthAuthenticator = ({
+    endpoint,
+    clientId,
+    clientSecret,
+    refreshThresholdSeconds = 15,
+}: {
+    endpoint: string;
+    clientId: string;
+    clientSecret: string;
+    refreshThresholdSeconds?: number;
+}) => {
+    let tokenPromise = createToken(endpoint, {
+        grant_type: 'client_credentials',
+        client_id: clientId,
+        client_secret: clientSecret,
+    });
+
+    const scheduleRefresh = () => {
+        tokenPromise.then((token) => {
+            const nowSeconds = Math.floor(Date.now() / 1000);
+            const refreshInSeconds = token.expires_at - nowSeconds - refreshThresholdSeconds;
+            setTimeout(() => {
+                tokenPromise = createToken(endpoint, {
+                    grant_type: 'refresh_token',
+                    client_id: clientId,
+                    client_secret: clientSecret,
+                    refresh_token: token.refresh_token,
+                });
+                scheduleRefresh();
+            }, refreshInSeconds * 1000);
+        });
+    };
+    scheduleRefresh();
+
+    return () => tokenPromise;
+};
+
+type TokenRequest = {
+    client_id: string;
+    client_secret: string;
+} & ({ grant_type: 'client_credentials' } | { grant_type: 'refresh_token'; refresh_token: string });
+
+type TokenResponse = {
+    access_token: string;
+    refresh_token: string;
+    expires_at: number; // epoch seconds
+};
+
+const createToken = async (endpoint: string, body: TokenRequest) => {
+    return withRetry(
+        exponentialBackoff(100),
+        5,
+    )(async () => {
+        const response = await fetch(endpoint, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: new URLSearchParams(body),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Failed to obtain OAuth token: ${await response.text()}`);
+        }
+
+        return response.json() as Promise<TokenResponse>;
+    });
+};
--- src/utils/retry.ts
+++ src/utils/retry.ts
@@ -1,17 +1,25 @@
+import { delay } from './delay';
 import { log } from './logger';
 
 export const withRetry =
-    (handleError: (error: unknown) => Promise<void>) =>
+    (handleError: (error: unknown, retry: number) => Promise<void>, maxRetries = 15) =>
     async <T>(func: () => Promise<T>): Promise<T> => {
         let lastError: unknown | undefined;
-        for (let i = 0; i < 15; i++) {
+        for (let i = 0; i < maxRetries; i++) {
             try {
                 return await func();
             } catch (error) {
-                await handleError(error);
+                await handleError(error, i + 1);
                 lastError = error;
             }
         }
         log.warn('Retries exhausted', { lastError });
         throw lastError;
+    };
+
+export const exponentialBackoff =
+    (initialDelayMs: number, maxDelayMs = 5_000) =>
+    async (_: unknown, retry: number) => {
+        const delayMs = Math.min(maxDelayMs, initialDelayMs * 2 ** retry);
+        await delay(delayMs);
     };
